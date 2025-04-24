1fedeb84295bb8db6ec438cc2e76dc4aa2e818c0
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-04-24 10:54:20 +0100

    Django fixtures

--- store/products/models.py
+++ store/products/models.py
@@ -1,9 +1,14 @@
+from idlelib.debugobj_r import remote_object_tree_item
+
 from django.db import models
 
 # Create your models here.
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -13,4 +18,8 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Product: {self.name}, category: {self.category}'
+
 
