80dc2070ab8f8095dd425ad909efba1f324cd8b4
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-04-25 12:21:56 +0100

    paginator

--- store/products/views.py
+++ store/products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from products.models import ProductCategory, Product, Basket
 from users.models import User
 # Create your views here.
@@ -11,9 +12,14 @@ def index(request):
     }
     return render(request, 'products/index.html', context)
 
-def products(request, category_id=None):
+def products(request, category_id=None, page_number=1):
     products = Product.objects.filter(category__id=category_id) if category_id else Product.objects.all()
-    context = {'title': 'Store - Каталог', 'categories': ProductCategory.objects.all(), 'products': products}
+    per_page = 1
+    paginator = Paginator(products, per_page)
+    products_paginator = paginator.page(page_number)
+    context = {'title': 'Store - Каталог',
+               'categories': ProductCategory.objects.all(),
+               'products': products_paginator}
     return render(request, 'products/products.html', context)
 
 
